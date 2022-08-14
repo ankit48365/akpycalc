9d88a421838cc12dccd702ab65cbf08404e02d13
ankit K <ankit48365@gmail.com>
2022-08-13 22:15:39 -0400

    0.0.2 version with init

---
+++ akpycalc/__init__.py
@@ -0,0 +1,4 @@
+from akpycalc import add
+from akpycalc import sub
+from akpycalc import mult
+from akpycalc import div
--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Ankiz Test PIP Package'
-LONG_DESCRIPTION = 'Demo Package - to add , sub, mult, div two numbers.'
+LONG_DESCRIPTION = 'Demo Package - to add , sub, mult, div two numbers. - version 0.0.2 - with init file'
 
 # Setting up
 setup(
