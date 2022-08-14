bd2df31d17aeba435637d6339ce179c5686e1ad3
ankit K <ankit48365@gmail.com>
2022-08-13 21:59:01 -0400

    pypi package

---
+++ akpycalc/akpycalc.py
@@ -0,0 +1,20 @@
+#creating MathOperation module
+#the module provides addition, subtraction, multiplication, and division functions
+
+#all the functions take two numbers as argument
+#creating addition function
+def add(num1,num2):
+    return num1+num2
+
+#creating subtraction function
+def sub(num1,num2):
+    return num1-num2 
+
+#creating multiplication function
+def mult(num1,num2):
+    return num1*num2
+
+
+#creating division function
+def div(num1,num2):
+    return num1/num2
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Ankiz Test PIP Package'
+LONG_DESCRIPTION = 'Demo Package - to add , sub, mult, div two numbers.'
+
+# Setting up
+setup(
+    name="akpycalc",
+    version=VERSION,
+    author="Ankiz",
+    author_email="<ankit48365@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    #install_requires=[''],
+    keywords=['python', 'ankiz', 'demo', 'ankitfotografia'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
